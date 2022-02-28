bec034e17aa65825a94b66d01c7bce379d6bf965
Farhan910 <farhanfaruk221@gmail.comm>
2022-03-01 01:54:31 +0600

    3rd

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const searching = () =>{
     const searchBox = document.getElementById('search-box').value
+    document.getElementById('search-box').value = "";
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchBox}`
     fetch(url)
     .then(response => response.json())
@@ -9,5 +10,25 @@ const searching = () =>{
 }
 
 const displayData = (phones) =>{
-    
+   const phone20 = phones.slice(0,20)
+   const  main = document.getElementById('main')
+   document.getElementById('main').innerHTML = "";
+   phone20.forEach((phone ) => {
+       console.log(phone);
+       const div = document.createElement('div')
+       div.classList.add('col-lg-4')
+       div.innerHTML = `
+       <div class="card mt-5 border-light " style="width: 18rem;">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">${phone.brand}</p>
+          <a href="#" class="btn btn-success">Details</a>
+        </div>
+      </div>
+       `
+       
+       main.appendChild(div)
+
+   })
 }
