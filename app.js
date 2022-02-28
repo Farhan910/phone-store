5aa4d289bf6d2855bda8f4c5b0a3186d7e2376ac
Farhan910 <farhanfaruk221@gmail.comm>
2022-03-01 01:01:19 +0600

    2nd

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hey');
+const searching = () =>{
+    const searchBox = document.getElementById('search-box').value
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchBox}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayData(data.data))
+
+
+}
+
+const displayData = (phones) =>{
+    
+}
