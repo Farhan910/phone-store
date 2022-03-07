18d157b5f05a8db6e532e02b97e27435007be1be
Farhan910 <farhanfaruk221@gmail.comm>
2022-03-08 01:28:57 +0600

    final

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
-document.getElementById("error-msg").style.display ="none"
+document.getElementById("error-msg").style.display = "none";
 const searching = () => {
   const searchBox = document.getElementById("search-box");
-  const searchInput = searchBox.value
-  
-  
-  
+  const searchInput = searchBox.value;
 
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchInput}`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => displayData(data.data))
-    
-  
+    .then((data) => displayData(data.data));
 };
-
-
 
 const displayData = (phones) => {
   console.log(phones);
- if (phones.length == 0 ){
-    document.getElementById("error-msg").style.display ="block"
-    document.getElementById('gif').style.display = 'block'
+  if (phones.length == 0) {
+    document.getElementById("error-msg").style.display = "block";
+    document.getElementById("gif").style.display = "block";
     document.getElementById("main").innerHTML = "";
     document.getElementById("details-main").innerHTML = "";
-    document.getElementById('search-box').value = "";
-  }
-  else{
+    document.getElementById("search-box").value = "";
+  } else {
     const phone20 = phones.slice(0, 20);
     const main = document.getElementById("main");
-    document.getElementById('foot').style.display="block"
-    document.getElementById("error-msg").style.display ="none"
-    document.getElementById('gif').style.display = 'none'
+    document.getElementById("foot").style.display = "block";
+    document.getElementById("error-msg").style.display = "none";
+    document.getElementById("gif").style.display = "none";
     document.getElementById("main").innerHTML = "";
     document.getElementById("details-main").innerHTML = "";
-    document.getElementById('search-box').value = "";
+    document.getElementById("search-box").value = "";
     phone20.forEach((phone) => {
       console.log(phone);
       const div = document.createElement("div");
@@ -49,14 +41,11 @@ const displayData = (phones) => {
           </div>
         </div>
          `;
-  
+
       main.appendChild(div);
     });
   }
-
-  
 };
-
 
 const details = (id) => {
   const url = `
@@ -71,7 +60,7 @@ const displayDetails = (details) => {
   const detailsMain = document.getElementById("details-main");
   document.getElementById("details-main").innerHTML = "";
   const div = document.createElement("div");
-console.log(details);
+  console.log(details);
   div.innerHTML = `
   <div class="card mb-3 mt-3 container border-light" style="max-width: 540px;">
   
@@ -83,25 +72,25 @@ console.log(details);
       
         <h2  class="card-title">${details.name}</h2>
         <h5 class="allName">Main features : </h5>
-        <p  class="card-text"> <span class="allName"> Display size :</span> <span class="text">${details.mainFeatures.displaySize}</span></p>
-        <p  class="card-text"> <span class="allName">Release date : </span><span class="text">${details.releaseDate}</span></p>
+        <p  class="card-text"><span class="allName"> Display size :</span> <span class="text">${details.mainFeatures.displaySize}</span></p>
+        <p  class="card-text"><span class="allName">Release date : </span><span class="text">${details.releaseDate}</span></p>
         <p  class="card-text"><span class="allName">Chip set : </span><span class="text">${details.mainFeatures.chipSet}</span></p>
         <p  class="card-text"><span class="allName">Storage : </span><span class="text">${details.mainFeatures.storage}</span></p>
         <p  class="card-text"><span class="allName">Memory : </span><span class="text">${details.mainFeatures.memory}</span></p>
         <p  class="card-text"><span class="allName">Sensors : </span><span class="text">${details.mainFeatures.sensors}</span> </p>
         <h5 class="allName">Others : </h5>
-        <p  class="card-text"><span class="allName">Bluetooth : </span><span class="text">${details.others.Bluetooth}</span></p>
-        <p  class="card-text"><span class="allName">GPS : </span><span class="text">${details.others.GPS}</span></p>
-        <p  class="card-text"><span class="allName">NFC : </span><span class="text">${details.others.NFC}</span></p>
-        <p  class="card-text"><span class="allName">Radio : </span><span class="text">${details.others.Radio}</span></p>
-        <p  class="card-text"><span class="allName">USB : </span><span class="text">${details.others.USB}</span></p>
-        <p  class="card-text"><span class="allName">WLAN : </span><span class="text">${details.others.WLAN}</span></p>
+        <p  class="card-text"><span class="allName">Bluetooth : </span><span class="text">${details.others?.Bluetooth ? details.others ?.Bluetooth : `no data found`}</span></p>
+        <p  class="card-text"><span class="allName">GPS : </span><span class="text">${details.others?.GPS ? details.others?.GPS : `no data found`}</span></p>
+        <p  class="card-text"><span class="allName">NFC : </span><span class="text">${details.others?.NFC ? details.others?.NFC : `no data found`}</span></p>
+        <p  class="card-text"><span class="allName">Radio : </span><span class="text">${details.others?.Radio ? details.others?.Radio : `no data found`}</span></p>
+        <p  class="card-text"><span class="allName">USB : </span><span class="text">${details.others?.USB ? details.others?.USB : `no data found`}</span></p>
+        <p  class="card-text"><span class="allName">WLAN : </span><span class="text">${details.others?.WLAN ? details.others?.WLAN : `no data found`}</span></p>
       </div>
     
 
  </div>
   `;
-  
+
   if (details.releaseDate == "") {
     div.innerHTML = `
    <div  class="card mb-3 mt-3 container border-light" style="max-width: 540px;">
